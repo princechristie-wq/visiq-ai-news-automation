1915b8783c20cf936486f7b10a8ad249a8c72e4e
princechristie-wq <prince.christie@gmail.com>
2026-06-14 17:08:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import os
+from groq import Groq
 
+client = Groq(
+    api_key=os.environ["GROQ_API_KEY"]
+)
+
+print("Starting...")
+
+topic_response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {
+            "role": "user",
+            "content": "Give one trending AI news topic. Return only the topic."
+        }
+    ]
+)
+
+topic = topic_response.choices[0].message.content.strip()
+
+with open(
+    "topic.txt",
+    "w",
+    encoding="utf-8"
+) as f:
+    f.write(topic)
+
+print("Topic:", topic)
+print("SUCCESS")
