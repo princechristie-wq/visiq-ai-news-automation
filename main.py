d02a9a0e1a028f89316cfe519954b3812d517e7f
princechristie-wq <prince.christie@gmail.com>
2026-06-14 18:07:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import os
 from groq import Groq
 import asyncio
 import edge_tts
+import textwrap
+import numpy as np
+import cv2
+
+from moviepy import (
+    AudioFileClip,
+    ImageClip,
+    TextClip,
+    CompositeVideoClip
+)
 
 client = Groq(
 api_key=os.environ["GROQ_API_KEY"]
@@ -15,7 +25,123 @@ async def create_voice(script):
         voice="en-US-ChristopherNeural",
         rate="+25%"
     )
+def create_background():
 
+    width = 1080
+    height = 1920
+
+    image = np.zeros(
+        (height, width, 3),
+        dtype=np.uint8
+    )
+
+    for y in range(height):
+
+        value = int(
+            20 + (y / height) * 80
+        )
+
+        image[y, :] = (
+            value,
+            value,
+            value + 20
+        )
+
+    cv2.rectangle(
+        image,
+        (0, 0),
+        (width, 180),
+        (0, 0, 180),
+        -1
+    )
+
+    cv2.putText(
+        image,
+        "BREAKING AI NEWS",
+        (60, 120),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        2,
+        (255, 255, 255),
+        5
+    )
+
+    cv2.imwrite(
+        "background.jpg",
+        image
+    )
+
+
+def create_video(topic):
+
+    audio = AudioFileClip(
+        "voice.mp3"
+    )
+
+    create_background()
+
+    background = (
+        ImageClip("background.jpg")
+        .with_duration(audio.duration)
+    )
+
+    headline_text = "\n".join(
+        textwrap.wrap(
+            topic,
+            width=18
+        )
+    )
+
+    headline = TextClip(
+        text=headline_text,
+        font_size=85,
+        color="white",
+        size=(900, None),
+        method="caption"
+    )
+
+    headline = (
+        headline
+        .with_duration(audio.duration)
+        .with_position(
+            ("center", 500)
+        )
+    )
+
+    brand = TextClip(
+        text="VISIQ AI",
+        font_size=60,
+        color="white"
+    )
+
+    brand = (
+        brand
+        .with_duration(audio.duration)
+        .with_position(
+            ("center", 1650)
+        )
+    )
+
+    final_video = CompositeVideoClip(
+        [
+            background,
+            headline,
+            brand
+        ],
+        size=(1080, 1920)
+    )
+
+    final_video = final_video.with_audio(
+        audio
+    )
+
+    final_video.write_videofile(
+        "final_video.mp4",
+        fps=24,
+        codec="libx264",
+        audio_codec="aac",
+        preset="ultrafast",
+        threads=2
+    )
     await communicate.save("voice.mp3")
 # =====================================
 
@@ -143,6 +269,8 @@ with open("hashtags.txt", "w", encoding="utf-8") as f:
 asyncio.run(
     create_voice(script)
 )
+
+create_video(topic)
 
 print("VOICE CREATED")
 print("SUCCESS")
