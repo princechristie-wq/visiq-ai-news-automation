ebc3790cd876362d0a17f28188b19573dce835e4
princechristie-wq <prince.christie@gmail.com>
2026-07-16 17:07:06 +0530

    Update video_engine.py

--- video_engine.py
+++ video_engine.py
@@ -35,3 +35,81 @@ BRAND_FONT_SIZE = 55
 def create_videos(visual_packages):
 
     pass
+
+def create_background():
+    width = 1080
+    height = 1920
+
+    image = np.zeros(
+        (height, width, 3),
+        dtype=np.uint8
+    )
+
+    for y in range(height):
+
+        r = int(
+            20 + (y / height) * 40
+        )
+
+        g = int(
+            20 + (y / height) * 20
+        )
+
+        b = int(
+            60 + (y / height) * 120
+        )
+
+        image[y, :] = (
+            b,
+            g,
+            r
+        )
+
+    for _ in range(150):
+
+        x = random.randint(
+            0,
+            width
+        )
+
+        y = random.randint(
+            0,
+            height
+        )
+
+        radius = random.randint(
+            2,
+            5
+        )
+
+        cv2.circle(
+            image,
+            (x, y),
+            radius,
+            (255, 255, 255),
+            -1
+        )
+
+    cv2.rectangle(
+        image,
+        (0, 1640),
+        (width, 1920),
+        (255, 60, 60),
+       -1
+)
+
+    cv2.putText(
+        image,
+        "VISIQ AI NEWS",
+        (50, 80),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        2,
+        (255, 255, 255),
+        4
+    )
+
+    cv2.imwrite(
+        "background.jpg",
+        image
+    )
+
